3ce44bf70e6e58c85684c2f79d72ef6db9915355
Teun Schuur <teun983@gmail.com>
2019-12-14 11:48:04 +0100

    new menu and full screen navigator

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Teun-Schuur.github.io
-website
+Just my website...
--- javaScript/main.js
+++ javaScript/main.js
@@ -1,10 +1,16 @@
 
-function setup(){
-  //createCanvas(100, 100)
-  noCanvas();
-}
-
-function draw(){
-  //background(0)
-
-}
+$(function(){
+    var flag = 0;
+    $('.menu-icon').on('click', function(e){
+      e.preventDefault();
+      if(flag == 0 ){
+        $('.menu-icon').addClass("active");
+        $('.menu').addClass('visible');
+        flag = 1;
+      }else {
+        $('.menu-icon').removeClass("active");
+        $('.menu').removeClass('visible');
+        flag = 0;
+      }
+   });
+});
